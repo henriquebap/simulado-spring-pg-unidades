64303474faa5d7b330dfe7bede6636be3c608496
henriquebap <henriquebaptista2003@gmail.com>
2024-04-16 08:52:01 -0300

    Usuario Entity JPA

--- src/main/java/br/com/fiap/unidades/entity/Usuario.java
+++ src/main/java/br/com/fiap/unidades/entity/Usuario.java
@@ -1,5 +1,15 @@
 package br.com.fiap.unidades.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,15 +20,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 
+@Entity
+@Table(name = "TB_SIMULADO_USUARIO", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_CPF", columnNames = "CPF_ALUNO"),
+        @UniqueConstraint(name = "UK_PESSOA_USERNAME", columnNames = {"ID_PESSOA", "USERNAME"})
+})
+
 
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_USUARIO")
+    @SequenceGenerator(name = "SQ_USUARIO", sequenceName = "SQ_USUARIO", allocationSize = 1)
+
     private Long id;
 
+    @Column(nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "ID_PESSOA", nullable = false)
     private Pessoa pessoa;
-
 }
