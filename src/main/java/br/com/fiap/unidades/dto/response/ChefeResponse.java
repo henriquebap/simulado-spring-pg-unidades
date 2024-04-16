2966f6e07a801baac437ea03a6ef43d134e1a187
henriquebap <henriquebaptista2003@gmail.com>
2024-04-16 09:48:44 -0300

    Unidade e Chefe Response

---
+++ src/main/java/br/com/fiap/unidades/dto/response/ChefeResponse.java
@@ -0,0 +1,18 @@
+package br.com.fiap.unidades.dto.response;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+
+@Builder
+public record ChefeResponse(
+
+    UnidadeResponse unidade,
+    LocalDateTime inicio,
+    LocalDateTime fim,
+    Long id,
+    UsuarioResponse usario,
+    Boolean substituto
+
+) {    
+}
