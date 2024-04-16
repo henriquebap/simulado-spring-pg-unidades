cd557b895a3cab7c0231e71a19719c464518f4d3
henriquebap <henriquebaptista2003@gmail.com>
2024-04-16 08:26:26 -0300

    Construindo UK - Chefe

--- src/main/java/br/com/fiap/unidades/entity/Chefe.java
+++ src/main/java/br/com/fiap/unidades/entity/Chefe.java
@@ -8,11 +8,21 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 
+@Entity
+@Table(name = "TB_SIMULADO_CHEFE", uniqueConstraints = {
+    @UniqueConstraint(name = "UK_USUARIO", columnNames = "USUARIO_CHEFE"),
+    @UniqueConstraint(name = "UK_UNIDADE", columnNames = "UNIDADE_CHEFE"),
+    @UniqueConstraint(name = "UK_DATA_FIM", columnNames = "DATA_FIM_CHEFE")
+})
 
 public class Chefe {
 
